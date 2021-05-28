f65702375d264e5b4ba9a49bda480531b6acb8ef
Aakash Sinha <aakash9868sinha@gmail.com>
2021-05-28 19:31:06 +0530

    Setting Up initial Project

---
+++ src/test/java/com/example/CreditSaisonINNLookup/CreditSaisonInnLookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CreditSaisonINNLookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditSaisonInnLookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
