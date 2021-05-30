15ce642278d6b9ce5d1bfad252eae07e732c11cb
Aakash Sinha <aakash9868sinha@gmail.com>
2021-05-30 18:56:53 +0530

    cors bug fix

--- src/main/java/com/example/CreditSaisonINNLookup/controller/CardSchemeController.java
+++ src/main/java/com/example/CreditSaisonINNLookup/controller/CardSchemeController.java
@@ -6,6 +6,7 @@ import com.example.CreditSaisonINNLookup.service.CardSchemeService;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +20,13 @@ public class CardSchemeController {
   @Autowired
   CardSchemeService cardSchemeService;
 
+  @CrossOrigin(origins = "http://localhost:3000")
   @GetMapping("verify/{bin}")
   public CommonResponseDTO<CardDetailDTO> verifyCardScheme(@PathVariable("bin") Long bin) {
     System.out.println("API CALLED");
     return cardSchemeService.verifyCardScheme(bin);
   }
-
+  @CrossOrigin(origins = "http://localhost:3000")
   @GetMapping("stats")
   public CommonResponseDTO<List<Map<Long, Long>>> getStats(@RequestParam("start") Long start,
       @RequestParam("limit") Long limit) {
